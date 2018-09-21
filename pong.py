68c003b1cde75acb31bfc20fcf67c125b2d9aba6
Michael  Chen <chen.young@Michaels-MacBook-Pro.local>
2018-09-20 19:49:29 -0700

    Comments and formatting

--- pong.py
+++ pong.py
@@ -4,27 +4,31 @@ import turtle
 import random
 wn = turtle.Screen()
 
-player = turtle.Turtle()
-wn.addshape('rect1.gif')
-player.shape('rect1.gif')
-player.speed(0)
 
-player2=turtle.Turtle()
-player2.shape('rect1.gif')
-player2.penup()
+wn.addshape('pong/rect1.gif')
+player = turtle.Turtle()
 player.penup()
+player.shape('pong/rect1.gif')
+player.speed(0)
 player.goto(-300,0)
 
-player2.goto(300,0)
+
+player2=turtle.Turtle()
+player2.penup()
+player2.shape('pong/rect1.gif')
 player2.speed(0)
+player2.goto(300,0)
+
+
 ball=turtle.Turtle()
-ball.shape('circle')
 ball.penup()
+ball.shape('circle')
+
 
 
 global t_dir, t2_dir
-t_dir = [0,0]
-t2_dir=[0,0]
+t_dir = [0,0]  # direction of player
+t2_dir = [0,0] # direction of player2
 
 
 def print_pos(x, y):
@@ -62,14 +66,17 @@ def bind():
     turtle.onkey(s, 's')
 
 bind()
-# unbind()
-wn.onclick(print_pos)
+# wn.onclick(print_pos) # for debugging
 player.penup()
-# player.goto(player.pos() + (1))
 ball_dir = [4*random.choice([-1,1]), random.choice([-1,1])*2]
+
+
 turtle.tracer(0,0)
+# https://stackoverflow.com/questions/16119991/how-to-speed-up-pythons-turtle-function-and-stop-it-freezing-at-the-end
 
 inc = 0
+# this is added to the upper bound of the random range every time the ball bounces
+# to make the ball bounce more random and fast as the round goes on
 
 
 time.sleep(1)
@@ -86,18 +93,31 @@ while True:
     ball.goto(ball.pos() + ball_dir)
 
 
-    # print(ball.xcor())
 
     if -260 < ball.xcor() < -255.0 and player.ycor()-90 < ball.ycor() < player.ycor() + 90 or 255 < ball.xcor() < 260 and \
             player2.ycor() - 90 < ball.ycor() < player2.ycor() + 90:
-        # print(inc)
+        # if ball hit paddle
+
         inc += .2
         ball_dir[0] = -1*ball_dir[0]
         sign = (ball_dir[1] > 0) - (ball_dir[1] < 0)
         vals = (sign * (2), sign * (inc + 4))
         ball_dir[1] = random.uniform(min(vals), max(vals))
+
+    if ball.ycor() < -290 or ball.ycor() > 300:
+        # if ball hit wall
+
+        inc += .2
+        ball_dir[1] = -1 * ball_dir[1]
+        sign = (ball_dir[1] > 0) - (ball_dir[1] < 0)
+        vals = (sign * (2), sign * (inc + 4))
+        ball_dir[1] = random.uniform(min(vals), max(vals))
+
     if ball.xcor() < -350 or ball.xcor() > 350:
-        unbind()
+        # if ball goes out
+
+        unbind() # don't let user input anything while resetting ... doesn't work
+
         if ball.xcor() < -350:
             s2 += 1
             print("Player 2 Won!")
@@ -105,27 +125,22 @@ while True:
             s1+=1
             print("Player 1 Won!")
         print("Score: {} {}".format(s1,s2))
+
         ball.goto(0,0)
         ball_dir = [random.choice([-1,1])*4, random.choice([-1,1])*2]
+
         inc=0
+
         t_dir = [0,0]
         t2_dir=[0,0]
 
         player.goto(-300, 0)
-
         player2.goto(300, 0)
+
         turtle.update()
         time.sleep(1)
         bind()
-    if ball.ycor() < -290 or ball.ycor() > 300:
-        inc += .2
-        ball_dir[1] = -1*ball_dir[1]
-        sign = (ball_dir[1] > 0) - (ball_dir[1] < 0)
-        vals = (sign*(2),sign*(inc+4))
-        ball_dir[1] = random.uniform(min(vals), max(vals))
 
-    turtle.update()
-    # print(player.pos())
-
+    turtle.update() # don't update until everything has moved
 
 wn.mainloop()
